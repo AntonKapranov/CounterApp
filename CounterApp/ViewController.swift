299650909ad35725723f3d8fcd549e974ee80e9a
got_sin <gr8toad@gmail.com>
2024-01-15 00:41:07 +0300

    Все задачи выполнены. Вёрстка без привяхок и Autolayout.

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -8,12 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //variables & funcs
+    private var counterValue:Int = 0
+    private var logValue:String = "История изменений:\n"
+    
+    //получение местного времени
+    private func getData()->String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        let formattedDate = dateFormatter.string(from: Date())
+        return "[\(formattedDate)]:"
+    }
+    
+    //функция увеличения
+    private func counterAdd(){
+        counterValue += 1
+        valueDisplay.text = "\(counterValue)"
+        logsDisplay.text += getData()
+        logsDisplay.text += " значение изменено на +1\n"
+    }
+    //функция уменьшения с проверкой на ноль
+    private func counterReduce(){
+        if counterValue != 0{
+            counterValue -= 1
+            valueDisplay.text = "\(counterValue)"
+            logsDisplay.text += getData()
+            logsDisplay.text += " значение изменено на -1\n"
+        } else{
+            logsDisplay.text += getData()
+            logsDisplay.text += " попытка уменьшить значение счётчика ниже 0\n"
+        }
+    }
+    
+    //функция сброса
+    private func counterReset(){
+        counterValue = 0
+        valueDisplay.text = "\(counterValue)"
+        logsDisplay.text += getData()
+        logsDisplay.text += " значение сброшено\n"
+    }
+    //Components
+    //Значение счётчика
+    @IBOutlet weak var valueDisplay: UILabel!
+    //кнопка сброса
+    @IBOutlet weak var buttonReset: UIButton!
+    //кнопка уменьшения
+    @IBOutlet weak var buttonReduce: UIButton!
+    //кнопка увеличения
+    @IBOutlet weak var buttonAdd: UIButton!
+    //история действий
+    @IBOutlet weak var logsDisplay: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        valueDisplay.text = "\(counterValue)"
+       logsDisplay.text = logValue
     }
-
-
+    
+    //действие – сброс
+    @IBAction func buttonResetAction(_ sender: Any) {
+        counterReset()
+    }
+    
+    //действие – увеличение
+    @IBAction func buttonAddAction(_ sender: Any) {
+        counterAdd()
+    }
+    
+    //действие – уменьшение
+    @IBAction func buttonReduceAction(_ sender: Any) {
+        counterReduce()
+    }
 }
 
