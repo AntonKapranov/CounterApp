00f11771d8a26d017952a88d26866f6ff9f8f628
got_sin <gr8toad@gmail.com>
2024-01-14 22:47:36 +0300

    Initial Commit

---
+++ CounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by Антон Капранов on 14.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
